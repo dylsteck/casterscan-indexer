d1602a89e6e632ef9440ac9e6768577e0ba45e6b
Dylan Steck <dylancsteck@gmail.com>
2023-05-06 22:13:04 -0400

    new deployment time

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import cron from 'node-cron';
 import { watch } from './lib.js';
-cron.schedule('*/30 * * * *', async () => {
+
+cron.schedule('30 * * * *', async () => {
     await watch()
-})
+});
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@ import cron from 'node-cron'
 
 import { watch } from './lib.js'
 
-cron.schedule('*/30 * * * *', async () => {
+cron.schedule('30 * * * *', async () => {
     await watch()
-})
+});
