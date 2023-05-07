c7a61e3eca8d6136a67ea9a8dca12e664772b045
Dylan Steck <dylancsteck@gmail.com>
2023-05-06 22:05:49 -0400

    one more typo fix

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,6 @@
 import 'dotenv/config';
 import cron from 'node-cron';
 import { watch } from './lib.js';
-cron.schedule('* * * * *', async () => {
-    await watch();
-});
+cron.schedule('*/30 * * * *', async () => {
+    await watch()
+})
